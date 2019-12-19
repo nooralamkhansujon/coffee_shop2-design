d20ca6f9da6b7d37c59841a59ab438e824d76491
Nooralam khan <CoreyMShafer@gmail.com>
2019-12-19 10:47:10 +0600

    Initial Commit

---
+++ main.js
@@ -0,0 +1,41 @@
+
+class Myfunc
+{
+  constructor(brand,category,model)
+  {
+       this.brand    = brand;
+       this.category = category;
+       this.model    = model;
+  }
+
+  get brandget ()
+  {
+    return this.brand;
+  }
+  get categoryget()
+  {
+     return this.category;
+  }
+  get modelget ()
+  {
+    return this.model;
+  }
+  set brandset(brand)
+  {
+    this.brand = brand;
+  }
+  set categoryset(category)
+  {
+    this.category = category;
+  }
+  set modelset (model)
+  {
+    this.model  = model;
+  }
+
+}
+
+const myfunc = new Myfunc('HP','computer','321Max');
+console.log(myfunc.brandget);
+myfunc.brandset = "Dell";
+console.log(myfunc.brandget);
